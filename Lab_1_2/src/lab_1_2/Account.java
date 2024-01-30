2206229288689a0b8ac0ac3488ae4672563df05e
AdamJ <adamjohnston144@gmail.com>
2024-01-30 07:45:03 -0500

    lab 1.2 complete

--- Lab_1_2/src/lab_1_2/Account.java
+++ Lab_1_2/src/lab_1_2/Account.java
@@ -5,21 +5,16 @@ import java.util.Date;
 public class Account {
     private int id = 0;
     private double balance = 0, annualInterestRate = 0; // in percent
-    private Date dateCreated;
+    private Date dateCreated = new Date(System.currentTimeMillis()); // sets date created to current time by default
 
-    Account(){
-        dateCreated = new Date(System.currentTimeMillis());
-    }
+    Account(){}
 
     Account(int id, double balance){
-        if(id < 0){
+        if(id < 0){ // if the id is less than 0, set the id to be positive and warn the user that you're doing so
             System.out.println("Warning: given ID is less than 0, forcing positive value.");
         }
-        if(balance < 0){
-            System.out.println("Warning: given balance is less than 0, forcing positive value.");
-        }
         this.id = Math.abs(id);
-        this.balance = Math.abs(balance);
+        this.balance = balance;
     }
 
     public int getId(){
@@ -51,14 +46,14 @@ public class Account {
     }
 
     public double getMonthlyInterest(){
-        return 0.0;
+        return balance * ((annualInterestRate / 100) / 12); // monthly interest rate = annualInterestRate / 12 here
     }
 
     public void withdraw(double amount){
-
+        balance -= amount;
     }
 
     public void deposit(double amount){
-
+        balance += amount;
     }
 }
--- Lab_1_2/src/lab_1_2/Lab_1_2.java
+++ Lab_1_2/src/lab_1_2/Lab_1_2.java
@@ -1,12 +1,37 @@
 package lab_1_2;
 
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Date;
 
 public class Lab_1_2 {
     public static void main(String[] args) {
-        //randomDemo();
-        //dateDemo();
+        Scanner input = new Scanner(System.in);
+        // Menu to run demos from
+        do { // Outer loop lets users run multiple demos in one session
+            int selection;
+            do { // Inner loop takes in user input and selects a demo to run based on that
+                System.out.println("Select a demo to run from the list below:\n Random Number Demo [0]\n Date Demo[1]\n Account Demo[2]");
+                selection = input.nextInt();
+                System.out.println(); // Spacing between menu and output of demo
+
+                switch (selection) {
+                    case 0:
+                        randomDemo();
+                        break;
+                    case 1:
+                        dateDemo();
+                        break;
+                    case 2:
+                        accountDemo();
+                        break;
+                    default:
+                        System.out.println("Invalid selection, please choose a demo from 0 to 2");
+                        break;
+                }
+            } while(selection < 0 || selection > 2);
+        System.out.println("\nDemo complete. Type \"end\" to exit the program, or enter any other character to run another demo.");
+        } while(!input.next().equalsIgnoreCase("end"));
     }
 
     public static void randomDemo(){
@@ -24,5 +49,16 @@ public class Lab_1_2 {
             date.setTime(i);
             System.out.println(date.toString());
         }
+    }
+
+    public static void accountDemo(){
+        Account account = new Account(1122, 20000); // create new account object
+        // test methods for account
+        account.setAnnualInterestRate(4.5);
+        account.withdraw(2500);
+        account.deposit(3000);
+        // print account information
+        System.out.printf("--- Account Details ---\n Balance: $%.2f\n Monthly Interest: $%.2f\n Account Creation Date: %s\n"
+        , account.getBalance(), account.getMonthlyInterest(), account.getDateCreated().toString());
     }
 }
