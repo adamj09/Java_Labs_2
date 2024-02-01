a6ae8ea5314260205fd4342629ee22de5e32dc10
AdamJ <adamjohnston144@gmail.com>
2024-02-01 13:48:00 -0500

    lab 2.2 added

---
+++ Lab_2_2/src/lab_2_2/DemoTownHouse.java
@@ -0,0 +1,49 @@
+package lab_2_2;
+
+import java.util.Scanner;
+
+public class DemoTownHouse {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        TownHouse[] houseArray = new TownHouse[3];
+
+        houseArray[0] = new TownHouse();
+        houseArray[0].setAddress(new Address(input.nextInt(), input.next(), input.next(), input.next(), input.next(), input.next()));
+        input.next();
+        houseArray[0].setBedCount(input.nextInt());
+        houseArray[0].setColor(input.next());
+        houseArray[0].setConstructionYear(input.nextInt());
+        houseArray[0].setDailyRent(input.nextDouble());
+        houseArray[0].setDiscountRate(input.nextDouble());
+
+        houseArray[1] = new TownHouse(
+            new Address(input.nextInt(), input.next(), input.next(), input.next(), input.next(), input.next()),
+            input.next(),
+            input.nextInt(),
+            input.nextInt()
+        );
+        input.next();
+
+        houseArray[1].setFloorCount(input.nextInt());
+        houseArray[1].setConstructionYear(input.nextInt());
+        houseArray[1].setIsWaterFront(input.nextBoolean());
+        houseArray[1].setDailyRent(input.nextDouble());
+        houseArray[1].setDiscountRate(input.nextDouble());
+
+        houseArray[2] = new TownHouse(
+            new Address(input.nextInt(), input.next(), input.next(), input.next(), input.next(), input.next()),
+            input.next(),
+            input.nextInt(),
+            input.nextInt(),
+            input.nextInt(),
+            input.nextInt(),
+            input.nextBoolean(),
+            input.nextDouble(),
+            input.nextDouble()
+        );
+
+        for(TownHouse townHouse : houseArray){
+            System.out.println(townHouse.toString());
+        }
+    }
+}
---
+++ Lab_2_2/src/lab_2_2/TownHouse.java
@@ -0,0 +1,130 @@
+package lab_2_2;
+
+public class TownHouse {
+    private Address address;
+
+    private String color;
+    private int bedCount, washroomCount, floorCount, constructionYear;
+    boolean isWaterFront;
+    double dailyRent, discountRate;
+
+    TownHouse(){
+        address = new Address();
+    }
+
+    TownHouse(Address address, String color, int bedCount, int washroomCount){
+        this.address = address;
+        this.color = color;
+        this.bedCount = bedCount;
+        this.washroomCount = washroomCount;
+    }
+
+    TownHouse(Address address, String color, int bedCount, int washroomCount, int floorCount, 
+        int constructionYear, boolean isWaterFront, double dailyRent, double discountRate){
+        this(address, color, bedCount, washroomCount);
+        this.floorCount = floorCount;
+        this.constructionYear = constructionYear;
+        this.isWaterFront = isWaterFront;
+        this.dailyRent = dailyRent;
+        this.discountRate = discountRate;
+    }
+
+    public Address getAddress(){
+        return address;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public int getBedCount(){
+        return bedCount;
+    }
+
+    public int getWashroomCount(){
+        return washroomCount;
+    }
+
+    public int getFloorCount(){
+        return floorCount;
+    }
+
+    public int getConstructionYear(){
+        return constructionYear;
+    }
+
+    public boolean getIsWaterFront(){
+        return isWaterFront;
+    }
+
+    public double getDailyRent(){
+        return dailyRent;
+    }
+
+    public double getDiscountRate(){
+        return discountRate;
+    }
+
+    public void setAddress(Address address){
+        this.address = address;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public void setBedCount(int bedCount){
+        this.bedCount = bedCount;
+    }
+
+    public void setWashroomCount(int washroomCount){
+        this.washroomCount = washroomCount;
+    }
+
+    public void setFloorCount(int floorCount){
+        this.floorCount = floorCount;
+    }
+
+    public void setConstructionYear(int constructionYear){
+        this.constructionYear = constructionYear;
+    }
+
+    public void setIsWaterFront(boolean isWaterFront){
+        this.isWaterFront = isWaterFront;
+    }
+
+    public void setDailyRent(double dailyRent){
+        this.dailyRent = dailyRent;
+    }
+
+    public void setDiscountRate(double discountRate){
+        this.discountRate = discountRate;
+    }
+
+    public String toString(){
+        return address.toString() + ", Color: " + color + ", Bed count: " + bedCount + ", Washroom count: " + washroomCount + 
+            ", Floor count: " + floorCount + ", Construction year: " + constructionYear + ", Is water front?: " + isWaterFront +
+            ", Daily rent: " + dailyRent + ", Discount rate: " + discountRate;
+    }
+}
+
+class Address {
+    public int civicNum;
+    public String street, city, postalCode, province, country;
+
+    Address(){}
+
+    Address(int civicNum, String street, String city, String postalCode, String province, String country){
+        this.civicNum = civicNum;
+        this.street = street;
+        this.city = city;
+        this.postalCode = postalCode;
+        this.province = province;
+        this.country = country;
+    }
+
+    public String toString(){
+        return civicNum + " " + street + ", " + city + ", " + postalCode + ", " + province + ", " + country;
+    }
+}
+
