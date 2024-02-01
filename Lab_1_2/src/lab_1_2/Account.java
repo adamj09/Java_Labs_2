b94738b6a0669bb60f88ed0637a89324cdf0cfdf
Adam Johnston <adamjohnston144@gmail.com>
2024-02-01 17:59:02 -0500

    fixes

--- Lab_1_2/src/lab_1_2/Account.java
+++ Lab_1_2/src/lab_1_2/Account.java
@@ -5,60 +5,56 @@ import java.util.Date;
 public class Account {
     private int id = 0;
     private double balance = 0, annualInterestRate = 0; // in percent
-    private Date dateCreated;
+    private Date dateCreated = new Date(System.currentTimeMillis());;
 
-    Account(){
-        dateCreated = new Date(System.currentTimeMillis());
-    }
+    Account() {} // default constructor
 
-    Account(int id, double balance){
-        if(id < 0){
+    Account(int id, double balance) {
+        // lets user know that input ID is invalid (and therefore will forcefully be changed to become valid)
+        if (id < 0) {
             System.out.println("Warning: given ID is less than 0, forcing positive value.");
         }
-        if(balance < 0){
-            System.out.println("Warning: given balance is less than 0, forcing positive value.");
-        }
         this.id = Math.abs(id);
-        this.balance = Math.abs(balance);
-    }
-
-    public int getId(){
-        return id;
-    }
-
-    public double getBalance(){
-        return balance;
-    }
-
-    public double getAnnualInterestRate(){
-        return annualInterestRate;
-    }
-
-    public Date getDateCreated(){
-        return dateCreated;
-    }
-
-    public void setId(int id){
-        this.id = id;
-    }
-
-    public void setBalance(double balance){
         this.balance = balance;
     }
 
-    public void setAnnualInterestRate(double annualInterestRate){
+    public int getId() {
+        return id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate) {
         this.annualInterestRate = annualInterestRate;
     }
 
-    public double getMonthlyInterest(){
-        return 0.0;
+    public double getMonthlyInterest() {
+        return balance * ((annualInterestRate / 100) / 12);
     }
 
-    public void withdraw(double amount){
-
+    public void withdraw(double amount) {
+        balance -= amount;
     }
 
-    public void deposit(double amount){
-
+    public void deposit(double amount) {
+        balance += amount;
     }
 }
--- Lab_1_2/src/lab_1_2/Lab_1_2.java
+++ Lab_1_2/src/lab_1_2/Lab_1_2.java
@@ -2,27 +2,72 @@ package lab_1_2;
 
 import java.util.Random;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Lab_1_2 {
     public static void main(String[] args) {
-        //randomDemo();
-        //dateDemo();
+        Scanner input = new Scanner(System.in);
+        // Menu to run demos from
+        do { // Outer loop lets users run multiple demos in one session
+            int selection;
+            do { // Inner loop takes in user input and selects a demo to run based on that
+                System.out.println(
+                        "Select a demo to run from the list below:\n Random Number Demo [0]\n Date Demo[1]\n Account Demo[2]");
+                selection = input.nextInt();
+                System.out.println(); // Spacing between menu and output of demo to avoid confusion
+
+                switch (selection) {
+                    case 0:
+                        randomDemo();
+                        break;
+                    case 1:
+                        dateDemo();
+                        break;
+                    case 2:
+                        accountDemo();
+                        break;
+                    default:
+                        System.out.println("Invalid selection, please choose a demo from 0 to 2");
+                        break;
+                }
+            } while (selection < 0 || selection > 2);
+
+            System.out.println(
+                    "\nDemo complete. Type \"end\" to exit the program, or enter any other character to run another demo.");
+        } while (!input.next().equalsIgnoreCase("end"));
     }
 
-    public static void randomDemo(){
+    public static void randomDemo() {
         Random random = new Random(1000);
 
-        for(int i = 0; i < 50; i++){
-            System.out.println(random.nextInt(100));
+        System.out.println("--- First 50 numbers generated (with seed 1000) between 0 and 100 ---");
+        for (int i = 0; i < 50; i++) {
+            System.out.printf("[%d] %d ", i + 1, random.nextInt(100));
+            if ((i + 1) % 10 == 0) { // new row every 10 entries
+                System.out.println();
+            }
         }
     }
 
-    public static void dateDemo(){
+    public static void dateDemo() {
+        System.out.println(
+                "Dates at the following times elapsed (ms) since 00:00:00, January 1, 1970, UTC+0 in local time:");
         Date date = new Date();
-
-        for(long i = 10000; i <= 100000000000L; i *= 10){
-            date.setTime(i);
-            System.out.println(date.toString());
+        for (int i = 4; i <= 11; i++) {
+            date.setTime((long) Math.pow(10, i)); // set the time
+            System.out.printf("%d: %s\n", (long) Math.pow(10, i), date.toString()); // display
         }
+    }
+
+    public static void accountDemo() {
+        Account account = new Account(1122, 20000);
+
+        account.setAnnualInterestRate(4.5);
+        account.withdraw(2500);
+        account.deposit(3000);
+
+        System.out.printf(
+                "--- Account Details ---\n Balance: $%.2f\n Monthly Interest: $%.2f\n Account Creation Date: %s\n",
+                account.getBalance(), account.getMonthlyInterest(), account.getDateCreated().toString());
     }
 }
