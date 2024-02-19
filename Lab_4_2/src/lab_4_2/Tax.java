bf8a0efe21078a919ed7c142b0f4ed1bf8b9faaa
AdamJ <adamjohnston144@gmail.com>
2024-02-19 13:07:53 -0500

    another tiny fix

--- Lab_4_2/src/lab_4_2/Tax.java
+++ Lab_4_2/src/lab_4_2/Tax.java
@@ -86,7 +86,7 @@ public class Tax {
     private int getTaxBracket(int filingStatus, double taxableIncome){
         int bracket = 0;
         for(bracket = 0; bracket < brackets[filingStatus].length; bracket++){
-            if(taxableIncome < brackets[filingStatus][bracket] && bracket != 5){
+            if(taxableIncome < brackets[filingStatus][bracket] && bracket != (brackets[filingStatus].length - 1)){
                 return bracket;
             }
         }
