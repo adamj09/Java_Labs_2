828d5bb2209c5bf00fc6a5debd46725ec396342b
AdamJ <adamjohnston144@gmail.com>
2024-02-19 13:22:16 -0500

    added comments to Tax class

--- Lab_4_2/src/lab_4_2/Tax.java
+++ Lab_4_2/src/lab_4_2/Tax.java
@@ -1,10 +1,16 @@
 package lab_4_2;
 
 public class Tax {
-    private final int SINGLE_FILER = 0, MARRIED_JOINTLY_OR_QUALIFYING_WIDOW = 1, MARRIED_SEPARATELY = 2, HEAD_OF_HOUSEHOLD = 3;
-    private int filingStatus = SINGLE_FILER;
- // dsfgfdhsf
-    private int[][] brackets = {
+    // Filing statuses
+    private final int 
+        SINGLE_FILER = 0, 
+        MARRIED_JOINTLY_OR_QUALIFYING_WIDOW = 1, 
+        MARRIED_SEPARATELY = 2, 
+        HEAD_OF_HOUSEHOLD = 3;
+    
+    private int filingStatus = SINGLE_FILER; // Set to default filing status
+
+    private int[][] brackets = { // Set default brackets to those defined in problem
         {
             25050,
             65550,
@@ -36,43 +42,45 @@ public class Tax {
     };
 
     private double[] rates = new double[5]; // Tax rates in percentages
-    private double taxableIncome = 0;
+    private double taxableIncome = 0; // Set default taxable income to 0
 
-    Tax(){}
+    Tax(){} // No-arg constructor
 
-    Tax(int filingStatus, int[][] brackets, double[] rates, double taxableIncome){
+    Tax(int filingStatus, int[][] brackets, double[] rates, double taxableIncome){ // Constructor with all args
         this.filingStatus = filingStatus;
         this.brackets = brackets.clone();
         this.rates = rates.clone();
         this.taxableIncome = taxableIncome;
     }
 
+    // --- Getters ---
     public int getFilingStatus() {
         return this.filingStatus;
-    }
-
-    public void setFilingStatus(int filingStatus) {
-        this.filingStatus = filingStatus;
     }
 
     public int[][] getBrackets() {
         return brackets.clone();
     }
 
-    public void setBrackets(int[][] brackets) {
-        this.brackets = brackets;
-    }
-
     public double[] getRates() {
         return rates.clone();
     }
 
-    public void setRates(double[] rates) {
-        this.rates = rates;
-    }
-
     public double getTaxableIncome() {
         return this.taxableIncome;
+    }
+
+    // --- Setters ---
+    public void setFilingStatus(int filingStatus) {
+        this.filingStatus = filingStatus;
+    }
+
+    public void setBrackets(int[][] brackets) {
+        this.brackets = brackets.clone();
+    }
+
+    public void setRates(double[] rates) {
+        this.rates = rates.clone();
     }
 
     public void setTaxableIncome(double taxableIncome) {
@@ -80,16 +88,18 @@ public class Tax {
     }
 
     public double getTax(){
+        // Returns amount that should be taxed based on filing status and income
         return taxableIncome * (rates[getTaxBracket(filingStatus, taxableIncome)] / 100);
     }
 
     private int getTaxBracket(int filingStatus, double taxableIncome){
         int bracket = 0;
-        for(; bracket < brackets[filingStatus].length; bracket++){
-            if(taxableIncome < brackets[filingStatus][bracket] && bracket != (brackets[filingStatus].length - 1)){
+        for(; bracket < brackets[filingStatus].length; bracket++){ // loop through all brackets
+            // If taxableIncome is less than max of a bracket and bracket has a max, return bracket
+            if(taxableIncome <= brackets[filingStatus][bracket] && bracket != (brackets[filingStatus].length - 1)){
                 return bracket;
             }
         }
-        return bracket;
+        return bracket; // This should return the maximum bracket (one with no limit)
     }
 }
