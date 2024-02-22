8a0a579c2e9146f6e776c96905ceb1aa381f65d6
Matt Cheung <matthewc0127@gmail.com>
2024-02-22 14:17:47 -0500

    Fixed it

--- Lab_4_2/src/lab_4_2/DriverTax.java
+++ Lab_4_2/src/lab_4_2/DriverTax.java
@@ -4,86 +4,86 @@ import java.util.Scanner;
 
 public class DriverTax {
     private static final int[][] brackets2001 = { // Set default brackets to 2001 brackets
-        {
-            27050,
-            65550,
-            136750,
-            297350,
-            297351
-        },
-        {
-            45200,
-            109250,
-            166500,
-            297350,
-            297351
-        },
-        {
-            22600,
-            54625,
-            83250,
-            148675,
-            148676
-        },
-        {
-            36250,
-            93650,
-            151650,
-            297350,
-            297351
-        },
+            {
+                    27050,
+                    65550,
+                    136750,
+                    297350,
+                    297351
+            },
+            {
+                    45200,
+                    109250,
+                    166500,
+                    297350,
+                    297351
+            },
+            {
+                    22600,
+                    54625,
+                    83250,
+                    148675,
+                    148676
+            },
+            {
+                    36250,
+                    93650,
+                    151650,
+                    297350,
+                    297351
+            },
     };
 
     private static final int[][] brackets2009 = { // Set default brackets to 2001 brackets
-        {
-            8350,
-            33950,
-            82250,
-            171550,
-            372950,
-            372951
-        },
-        {
-            16700,
-            67900,
-            137050,
-            208850,
-            372950,
-            372951
-        },
-        {
-            8350,
-            33950,
-            68525,
-            104425,
-            186475,
-            186476
-        },
-        {
-            11950,
-            45500,
-            117450,
-            190200,
-            372950,
-            372951
-        },
+            {
+                    8350,
+                    33950,
+                    82250,
+                    171550,
+                    372950,
+                    372951
+            },
+            {
+                    16700,
+                    67900,
+                    137050,
+                    208850,
+                    372950,
+                    372951
+            },
+            {
+                    8350,
+                    33950,
+                    68525,
+                    104425,
+                    186475,
+                    186476
+            },
+            {
+                    11950,
+                    45500,
+                    117450,
+                    190200,
+                    372950,
+                    372951
+            },
     };
 
     private static final double[] rates2001 = {
-        15,
-        27.5,
-        30.5,
-        35.5,
-        39.1
+            15,
+            27.5,
+            30.5,
+            35.5,
+            39.1
     };
 
     private static final double[] rates2009 = {
-        10,
-        15,
-        25,
-        28,
-        33,
-        35
+            10,
+            15,
+            25,
+            28,
+            33,
+            35
     };
 
     public static void main(String[] args) {
@@ -91,7 +91,7 @@ public class DriverTax {
         Tax tax = new Tax();
 
         int choice;
-        while(true){
+        while (true) {
             // Get choice from user
             System.out.println("--- Choose an option ---\n1 - Display increment for 2001 tax brackets" +
                     "\n2 - Display increment for 2009 tax brackets\n3 - User input\n4 - Quit");
@@ -99,7 +99,8 @@ public class DriverTax {
 
             switch (choice) {
                 case 1: // 2001 tax brackets
-                    // Print tax for each filing status for income between $50,000 and $60,000, with increments of $1,000
+                    // Print tax for each filing status for income between $50,000 and $60,000, with
+                    // increments of $1,000
                     tax.setBrackets(brackets2001);
                     tax.setRates(rates2001);
                     for (int j = 0; j < 4; j++) { // cycle through filing statuses
@@ -134,61 +135,61 @@ public class DriverTax {
                     do {
                         System.out.println("Enter the tax year (2001 and 2009 supported): ");
                         taxYear = input.nextInt();
-                        if(taxYear != 2001 && taxYear != 2009){
+                        if (taxYear != 2001 && taxYear != 2009) {
                             System.out.println("Invalid tax year. Please enter 2001 or 2009.");
                         }
-                    } while(taxYear != 2001 && taxYear != 2009);
+                    } while (taxYear != 2001 && taxYear != 2009);
 
-                    switch(taxYear){
+                    switch (taxYear) {
                         case 2001:
-                                tax.setBrackets(brackets2001);
-                                tax.setRates(rates2001);
-                                do {
-                                    System.out.println("--- Enter the Filing Status ---\n0 - Single Filer\n"
-                                            + "1 - Married Jointly or Qualifying Widow(er)\n2 - Married Separately\n3 - Head of Household");
-                                    tax.setFilingStatus(input.nextInt());
-                                    if (tax.getFilingStatus() < 0 || tax.getFilingStatus() > 3) {
-                                        System.out.println("Invalid filing status. Please enter 0, 1, 2 or 3.");
-                                    }
-                                } while (tax.getFilingStatus() < 0 || tax.getFilingStatus() > 3);
-                            
-                                // Get taxable income from user
-                                do {
-                                    System.out.println("Enter the taxable income: ");
-                                    tax.setTaxableIncome(input.nextDouble());
-                                    if (tax.getTaxableIncome() < 0) {
-                                        System.out.println("Income cannot be negative.");
-                                    }
-                                } while (tax.getTaxableIncome() < 0);
-                                System.out.println("Tax Year: 2001, Filing Status: " + 
+                            tax.setBrackets(brackets2001);
+                            tax.setRates(rates2001);
+                            do {
+                                System.out.println("--- Enter the Filing Status ---\n0 - Single Filer\n"
+                                        + "1 - Married Jointly or Qualifying Widow(er)\n2 - Married Separately\n3 - Head of Household");
+                                tax.setFilingStatus(input.nextInt());
+                                if (tax.getFilingStatus() < 0 || tax.getFilingStatus() > 3) {
+                                    System.out.println("Invalid filing status. Please enter 0, 1, 2 or 3.");
+                                }
+                            } while (tax.getFilingStatus() < 0 || tax.getFilingStatus() > 3);
+
+                            // Get taxable income from user
+                            do {
+                                System.out.println("Enter the taxable income: ");
+                                tax.setTaxableIncome(input.nextDouble());
+                                if (tax.getTaxableIncome() < 0) {
+                                    System.out.println("Income cannot be negative.");
+                                }
+                            } while (tax.getTaxableIncome() < 0);
+                            System.out.println("Tax Year: 2001, Filing Status: " +
                                     tax.getFilingStatusesAsStrings()[tax.getFilingStatus()]); // header
                             break;
                         case 2009:
-                                System.out.println("--- Tax Year: 2009 ---"); // header
-                                tax.setBrackets(brackets2009);
-                                tax.setRates(rates2009);
-                                do {
-                                    System.out.println("--- Enter the Filing Status ---\n0 - Single Filer\n"
-                                            + "1 - Married Jointly or Qualifying Widow(er)\n2 - Married Separately\n3 - Head of Household");
-                                    tax.setFilingStatus(input.nextInt());
-                                    if (tax.getFilingStatus() < 0 || tax.getFilingStatus() > 3) {
-                                        System.out.println("Invalid filing status. Please enter 0, 1, 2 or 3.");
-                                    }
-                                } while (tax.getFilingStatus() < 0 || tax.getFilingStatus() > 3);
-                            
-                                // Get taxable income from user
-                                do {
-                                    System.out.println("Enter the taxable income: ");
-                                    tax.setTaxableIncome(input.nextDouble());
-                                    if (tax.getTaxableIncome() < 0) {
-                                        System.out.println("Income cannot be negative.");
-                                    }
-                                } while (tax.getTaxableIncome() < 0);
-                                System.out.println("Tax Year: 2009, Filing Status: " + 
+                            System.out.println("--- Tax Year: 2009 ---"); // header
+                            tax.setBrackets(brackets2009);
+                            tax.setRates(rates2009);
+                            do {
+                                System.out.println("--- Enter the Filing Status ---\n0 - Single Filer\n"
+                                        + "1 - Married Jointly or Qualifying Widow(er)\n2 - Married Separately\n3 - Head of Household");
+                                tax.setFilingStatus(input.nextInt());
+                                if (tax.getFilingStatus() < 0 || tax.getFilingStatus() > 3) {
+                                    System.out.println("Invalid filing status. Please enter 0, 1, 2 or 3.");
+                                }
+                            } while (tax.getFilingStatus() < 0 || tax.getFilingStatus() > 3);
+
+                            // Get taxable income from user
+                            do {
+                                System.out.println("Enter the taxable income: ");
+                                tax.setTaxableIncome(input.nextDouble());
+                                if (tax.getTaxableIncome() < 0) {
+                                    System.out.println("Income cannot be negative.");
+                                }
+                            } while (tax.getTaxableIncome() < 0);
+                            System.out.println("Tax Year: 2009, Filing Status: " +
                                     tax.getFilingStatusesAsStrings()[tax.getFilingStatus()]); // header
                             break;
                     }
-                    
+
                     // Display tax rate and taxed income
                     System.out.printf("Tax is %.2f%% ($%.2f)\n", tax.getTaxRate(), tax.getTax());
                     break;
@@ -200,23 +201,6 @@ public class DriverTax {
                     System.out.println("Invalid choice. Please enter 1, 2, or 3.");
                     break;
             }
-        }   
+        }
     }
-
-  /* private static double getInputFromUser(String promptMessage, String errorMessage, double[] validInputs){
-        Scanner input = new Scanner(System.in);
-        double value;
-        boolean isValid = false;
-        do {
-            System.out.println(promptMessage);
-            value = input.nextDouble();
-
-            for(int i = 0; i < validInputs.length i++){
-
-            }
-            if (tax.getTaxableIncome() < 0) {
-                System.out.println(errorMessage);
-            }
-        } while (tax.getTaxableIncome() < 0);
-    }*/
 }
