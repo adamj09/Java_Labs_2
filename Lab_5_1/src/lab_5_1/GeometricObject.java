6816d98fd1126bda69a1e053223676956eadbe60
AdamJ <adamjohnston144@gmail.com>
2024-02-29 13:00:33 -0500

    added skeleton classes

--- Lab_5_1/src/lab_5_1/GeometricObject.java
+++ Lab_5_1/src/lab_5_1/GeometricObject.java
@@ -1,5 +1,43 @@
 package lab_5_1;
 
+import java.util.Date;
+
 public class GeometricObject {
-    
+    private String color;
+    private boolean filled;
+    private Date dateCreated;
+
+    public GeometricObject(){
+        dateCreated = new Date();
+    }
+
+    public GeometricObject(String color, boolean filled) {
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isFilled() {
+        return filled;
+    }
+
+    public void setFilled(boolean filled) {
+        this.filled = filled;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
 }
--- Lab_5_1/src/lab_5_1/Triangle.java
+++ Lab_5_1/src/lab_5_1/Triangle.java
@@ -1,10 +1,39 @@
 package lab_5_1;
 
 public class Triangle extends GeometricObject{
-    double side1 = 1, side2 = 1, side3 = 1;
+    private double side1 = 1, side2 = 1, side3 = 1;
 
     Triangle(){}
-    Triangle(double side1, double side2, double side3){
-        
+    Triangle(double side1, double side2, double side3, String color, boolean filled){
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+        setColor(color);
+        setFilled(filled);
     }
+
+    public double getSide1() {
+        return this.side1;
+    }
+
+    public double getSide2() {
+        return this.side2;
+    }
+
+    public double getSide3() {
+        return this.side3;
+    }
+
+    public void setSide1(double side1) {
+        this.side1 = side1;
+    }
+
+    public void setSide2(double side2) {
+        this.side2 = side2;
+    }
+
+    public void setSide3(double side3) {
+        this.side3 = side3;
+    }
+
 }
