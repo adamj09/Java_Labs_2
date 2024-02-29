d3ae47d31fcd274e0a5b3a7b3ce887239885494e
AdamJ <adamjohnston144@gmail.com>
2024-02-29 12:42:11 -0500

    added Triangle and GeometricObject classes

---
+++ Lab_5_1/src/lab_5_1/Triangle.java
@@ -0,0 +1,10 @@
+package lab_5_1;
+
+public class Triangle extends GeometricObject{
+    double side1 = 1, side2 = 1, side3 = 1;
+
+    Triangle(){}
+    Triangle(double side1, double side2, double side3){
+        
+    }
+}
