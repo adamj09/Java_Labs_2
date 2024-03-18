896675a188075b6178daecf4aea070af89174ae1
AdamJ <adamjohnston144@gmail.com>
2024-03-18 13:19:17 -0400

    done Q2 (maybe)

--- Lab_6_1/src/lab_6_1/Q1.java
+++ Lab_6_1/src/lab_6_1/Q1.java
@@ -3,12 +3,12 @@ package lab_6_1;
 import java.util.ArrayList;
 
 public class Q1 {
-    public static void randomSquareMatrix(int sideLength){
+    public static void randomSquareMatrix(int sideLength) {
         ArrayList<Integer> columns = new ArrayList<>();
         ArrayList<Integer> rows = new ArrayList<>();
 
         for(int i = 0; i < sideLength; i++){
-            
+
         }
     }
 }
