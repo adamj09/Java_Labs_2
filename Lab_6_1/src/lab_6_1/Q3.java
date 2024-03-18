d3f79a36ffafbba5874bb39b199ec1ddd914f57b
AdamJ <adamjohnston144@gmail.com>
2024-03-18 13:22:07 -0400

    Q3 done (maybe)

--- Lab_6_1/src/lab_6_1/Q3.java
+++ Lab_6_1/src/lab_6_1/Q3.java
@@ -1,5 +1,12 @@
 package lab_6_1;
 
+import java.util.ArrayList;
+
 public class Q3 {
-    
+    public static ArrayList<Integer> union(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+        ArrayList<Integer> union = new ArrayList<>();
+        union.addAll(list1);
+        union.addAll(list2);
+        return union;
+    }
 }
