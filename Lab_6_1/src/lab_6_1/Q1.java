34279d6d051948de8905eb4a8c16fdb5adfbafe4
AdamJ <adamjohnston144@gmail.com>
2024-03-18 13:04:02 -0400

    done Q4

--- Lab_6_1/src/lab_6_1/Q1.java
+++ Lab_6_1/src/lab_6_1/Q1.java
@@ -1,5 +1,14 @@
 package lab_6_1;
 
+import java.util.ArrayList;
+
 public class Q1 {
-    
+    public static void randomSquareMatrix(int sideLength){
+        ArrayList<Integer> columns = new ArrayList<>();
+        ArrayList<Integer> rows = new ArrayList<>();
+
+        for(int i = 0; i < sideLength; i++){
+            
+        }
+    }
 }
--- Lab_6_1/src/lab_6_1/Q4.java
+++ Lab_6_1/src/lab_6_1/Q4.java
@@ -1,5 +1,13 @@
 package lab_6_1;
 
+import java.util.ArrayList;
+
 public class Q4 {
-    
+    public static ArrayList<Character> toCharArray(String string) {
+        ArrayList<Character> array = new ArrayList<>();
+        for(int i = 0; i < string.length(); i++) {
+            array.add(string.charAt(i));
+        }
+        return array;
+    }
 }
