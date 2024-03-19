a1cf35cde4db09d9aa89f5d7f01314b8f99027d2
Adam Johnston <adamjohnston144@gmail.com>
2024-03-18 20:42:09 -0400

    done lab 6.1

--- Lab_6_1/src/lab_6_1/Driver.java
+++ Lab_6_1/src/lab_6_1/Driver.java
@@ -7,8 +7,8 @@ public class Driver {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         while(true){ // Run app (with menu)
-            System.out.println("--- Demos ---\n [1] Maximum Value Demo\n [2] Shuffle Values Demo\n "+ 
-                "[3] Sum Values Demo\n [4] To Character Array\n [5] exit");
+            System.out.println("--- Demos ---\n [1] Largest Rows and Columns\n [2] Sort\n "+ 
+                "[3] Union Arrays\n [4] To Character Array\n [5] Exit");
             
             // Get choice of demo from user
             int choice = input.nextInt();
@@ -19,16 +19,20 @@ public class Driver {
 
             switch (choice) {
                 case 1:
-                    
+                    largestRowsAndColumnsTest();
+                    System.out.println();
                     break;
                 case 2:
-                    
+                    sortTest();
+                    System.out.println();
                     break;
                 case 3:
-                    
+                    unionTest();
+                    System.out.println();
                     break;
                 case 4:
                     toCharacterArrayTest();
+                    System.out.println();
                     break;
                 default:
                     System.exit(0);
@@ -37,10 +41,85 @@ public class Driver {
         }
     }
 
+    public static void largestRowsAndColumnsTest() {
+        Scanner input = new Scanner(System.in);
+        // Get the matrix's side length from the user
+        int sideLength = 0;
+        do {
+            System.out.println("Enter the square matrix's side length: ");
+            sideLength = input.nextInt();
+            if(sideLength <= 0) {
+                System.out.println("Invalid side length: must be greater than 0!");
+            }
+        } while(sideLength <= 0);
+
+        // Create and print matrix
+        int[][] matrix = Q1.randomSquareMatrix(sideLength);
+        Q1.printMatrix(matrix);
+
+        // Get largest rows and columns and print them
+        ArrayList<Integer> largestRows = Q1.largestRows(matrix);
+        ArrayList<Integer> largestColumns = Q1.largestColumns(matrix);
+
+        System.out.print("Largest rows: ");
+        for(int i = 0; i < largestRows.size(); i++) {
+            System.out.print(largestRows.get(i) + " ");
+        }
+
+        System.out.print("Largest columns: ");
+        for(int i = 0; i < largestColumns.size(); i++) {
+            System.out.print(largestColumns.get(i) + " ");
+        }
+    }
+
+    public static void sortTest() {
+        Scanner input = new Scanner(System.in);
+        ArrayList<Integer> array = new ArrayList<>();
+
+        // Get digits from user
+        System.out.println("Enter a five integers to sort: ");
+        for(int i = 0; i < 5; i++) {
+            array.add(input.nextInt());
+        }
+
+        // Sort and display sorted digits
+        Q2.sort(array);
+        System.out.println("Sorted array: ");
+        for(int i = 0; i < array.size(); i++) {
+            System.out.print(array.get(i) + " ");
+        }
+    }
+
+    public static void unionTest() {
+        Scanner input = new Scanner(System.in);
+
+        // Get arrays of integers from user
+        System.out.println("Enter five integers for array 1: ");
+        ArrayList<Integer> array1 = new ArrayList<>();
+        for(int i = 0; i < 5; i++) {
+            array1.add(input.nextInt());
+        }
+
+        System.out.println("Enter five integers for array 2: ");
+        ArrayList<Integer> array2 = new ArrayList<>();
+        for(int i = 0; i < 5; i++) {
+            array2.add(input.nextInt());
+        }
+
+        // Create and display unioned array
+        ArrayList<Integer> unioned = Q3.union(array1, array2);
+        System.out.print("Unioned array: ");
+        for(int i = 0; i < unioned.size(); i++) {
+            System.out.print(unioned.get(i) + " ");
+        }
+    }
+
     public static void toCharacterArrayTest() {
         Scanner input = new Scanner(System.in);
+        // Get string from user
         System.out.println("Enter a string to convert to a Character array: ");
         ArrayList<Character> array = Q4.toCharacterArray(input.next());
+        // Display the string as separate characters from array
         for(int i = 0; i < array.size(); i++) {
             System.out.print(array.get(i) + " ");
         }
--- Lab_6_1/src/lab_6_1/Q1.java
+++ Lab_6_1/src/lab_6_1/Q1.java
@@ -3,15 +3,69 @@ package lab_6_1;
 import java.util.ArrayList;
 
 public class Q1 {
-    public static void randomSquareMatrix(int sideLength) {
+    public static ArrayList<Integer> largestColumns(int[][] matrix) {
         ArrayList<Integer> columnIndices = new ArrayList<>();
+
+        int previousColumnSum = 0;
+        for(int i = 0; i < matrix[0].length; i++){ // Do this for every column
+            int columnSum = 0;
+            for(int j = 0; j < matrix.length; j++){ // Get sum for current column
+                columnSum += matrix[j][i];
+            }
+            
+            if(columnSum > previousColumnSum) { // Remove all indices and add new max to list
+                columnIndices.clear();
+                columnIndices.add(i);
+                previousColumnSum = columnSum;
+            }
+            else if(columnSum == previousColumnSum) { // Add to index to list if there are 2 or more maxes
+                columnIndices.add(i);
+            }
+        }
+
+        return columnIndices;
+    }
+
+    public static ArrayList<Integer> largestRows(int[][] matrix) {
         ArrayList<Integer> rowIndices = new ArrayList<>();
 
+        int previousRowSum = 0;
+        for(int i = 0; i < matrix.length; i++){ // Do this for every row
+            int rowSum = 0;
+            for(int j = 0; j < matrix[i].length; j++){ // Get sum for current row
+                rowSum += matrix[i][j];
+            }
+
+            if(rowSum > previousRowSum) { // Remove all indices and add new max to list
+                rowIndices.clear();
+                rowIndices.add(i);
+                previousRowSum = rowSum;
+            }
+            else if(rowSum == previousRowSum) { // Add to index to list if there are 2 or more maxes
+                rowIndices.add(i);
+            }
+        }
+
+        return rowIndices;
+    }
+
+    public static int[][] randomSquareMatrix(int sideLength) {
         int[][] matrix = new int[sideLength][sideLength];
         for(int i = 0; i < matrix.length; i++) {
             for(int j = 0; j < matrix[i].length; j++) {
                 matrix[i][j] = (int)Math.round(Math.random());
             }
         }
+        return matrix;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
