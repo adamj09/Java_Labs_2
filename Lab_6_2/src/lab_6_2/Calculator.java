6436110f6a7b1611706268b1a3768beb4d1a8755
AdamJ <adamjohnston144@gmail.com>
2024-03-21 12:39:03 -0400

    added q1

---
+++ Lab_6_2/src/lab_6_2/Calculator.java
@@ -0,0 +1,58 @@
+package lab_6_2;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Calculator {
+    public static void runCalculatorApp() {
+        Scanner input = new Scanner(System.in);
+        while(true) {
+            double num1 = 1, num2 = 1;
+            String operator = "";
+            while(true) {
+                try {
+                    num1 = input.nextDouble();
+                    operator = input.next();
+                    num2 = input.nextDouble();
+                    break;
+                }
+                catch (InputMismatchException ex) {
+                    System.out.println("Invalid formatting. Format should be: [double] [operator] [double].");
+                    input.nextLine();
+                }
+            }
+            
+            switch (operator) {
+                case "+":
+                    System.out.println(" = " + (num1 + num2));
+                    break;
+                case "-":
+                    System.out.println(" = " + (num1 - num2));
+                    break;
+                case "*":
+                    System.out.println(" = " + (num1 * num2));
+                    break;
+                case "/":
+                    try {
+                        System.out.println(" = " + divide(num1, num2));
+                    }
+                    catch (ArithmeticException ex) {
+                        System.out.println("Cannot divide by zero.");
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid operator.");
+                    break;
+            }
+            System.out.println("Operation complete. Enter \"exit\" to exit");
+            input.next();
+        }
+    }
+
+    public static double divide(double dividend, double divisor) throws ArithmeticException {
+        if(divisor == 0) {
+            throw new ArithmeticException("Cannot divide by zero.");
+        }
+        return dividend / divisor;
+    }
+}
--- Lab_6_2/src/lab_6_2/Lab_6_2.java
+++ Lab_6_2/src/lab_6_2/Lab_6_2.java
@@ -2,6 +2,6 @@ package lab_6_2;
 
 public class Lab_6_2 {
     public static void main(String[] args) {
-        
+        Calculator.runCalculatorApp();
     }
 }
