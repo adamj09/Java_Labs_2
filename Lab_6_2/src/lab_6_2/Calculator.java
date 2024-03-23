709b544e9f436872591a01bb2ad80a5510ecbab8
Adam Johnston <adamjohnston144@gmail.com>
2024-03-23 12:06:12 -0400

    finished actually good calculator

--- Lab_6_2/src/lab_6_2/Calculator.java
+++ Lab_6_2/src/lab_6_2/Calculator.java
@@ -5,65 +5,66 @@ import java.util.ArrayList;
 
 public class Calculator {
     // Store previous answer to make additional operations with
-    private Double previousAnswer = null;
+    private static Double previousAnswer = null;
 
-    public void runCalculatorApp() {
+    public static void runCalculatorApp() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Here's a basic calculator. Enter \"help\" for the list of commands and operators.");
 
         while(true) {
-            StringBuilder input = new StringBuilder(scanner.nextLine());
+            String input = scanner.nextLine();
 
             switch (input.toString().toLowerCase()) {
                 case "help": // Provide list of commands and operators to user
                     displayHelp();
-                    continue;
-                case "c": // Clear the calculator
+                    break;
+                case "c": // Clear the previous answer
                     previousAnswer = null;
-                    continue;
+                    break;
                 case "e": // Exit the calculator
                     System.exit(0);
                     break;
                 default: // Run an operation
-                    try {
-                        previousAnswer = calculate(input);
-                        System.out.println(previousAnswer == null ? "": " = " + previousAnswer);
-                    }
-                    catch (ArithmeticException ex) {
-                        System.out.println("Cannot divide by zero.");
+                    Double answer = calculate(input);
+                    if(answer != null) {
+                        previousAnswer = answer;
+                        System.out.printf(" = %.10f\n", previousAnswer);
                     }
                     break;
             }
         }
     }
 
-    private Double calculate(StringBuilder input) {
+    private static Double calculate(String input) {
         ArrayList<String> parsedInput = parseInput(input);
-        String operator = "";
-        double value1 = 0, value2 = 0;
-
-        // Set values properly
-        try {
-            value1 = Double.parseDouble(parsedInput.get(0));
-        }
-        catch(NumberFormatException ex) {
-            if(previousAnswer == null) {
-                System.out.println("Invalid input. Bad formatting. Type \"help\" for more information.");
-                return null;
-            }
-            value1 = previousAnswer;
-        }
-
-        try {
-            operator = previousAnswer == null ? parsedInput.get(1): parsedInput.get(0);
-            value2 = previousAnswer == null ? Double.parseDouble(parsedInput.get(2)): Double.parseDouble(parsedInput.get(1));
-        }
-        catch(Exception ex) {
+        // Make sure there were no parsing errors
+        if(parsedInput == null) {
             System.out.println("Invalid input. Bad formatting. Type \"help\" for more information.");
             return null;
         }
 
+        String operator = "";
+        double value1 = 0, value2 = 0;
+
+        // Set values accordingly
+        if(parsedInput.size() > 2) {
+            previousAnswer = null;
+            value1 = Double.parseDouble(parsedInput.get(0));
+            value2 = Double.parseDouble(parsedInput.get(1));
+            operator = parsedInput.get(2);
+        }
+        else if(previousAnswer != null) {
+            value1 = previousAnswer;
+            value2 = Double.parseDouble(parsedInput.get(0));
+            operator = parsedInput.get(1);
+        }
+        else {
+            System.out.println("Invalid input. Bad formatting. Type \"help\" for more information.");
+            return null;
+        }
+
+        // Return result of operation
         switch (operator) {
             case "+":
                 return value1 + value2;
@@ -72,57 +73,92 @@ public class Calculator {
             case "*":
                 return value1 * value2;
             case "/":
-                return divide(value1, value2);
+                try {
+                    return divide(value1, value2);
+                }
+                catch(ArithmeticException ex) {
+                    System.out.println("Cannot divide by zero.");
+                }
             default:
                 System.out.println("Invalid operator.");
                 return null;
         }
     }
 
-    //TODO: Handle decimal numbers
-    private ArrayList<String> parseInput(StringBuilder input) {
-        ArrayList<Double> digits = new ArrayList<>();
-        ArrayList<String> parsed = new ArrayList<>();
+    private static ArrayList<String> parseInput(String input) {
+        ArrayList<String> inputArray = new ArrayList<>();
 
-        boolean operatorFound = false;
+        // Split input into array for easier parsing
         for(int i = 0; i < input.length(); i++) {
-            try {
-                digits.add(Double.parseDouble(input.charAt(i) + "")); // Parse digits from input
+            if(input.charAt(i) == ' ') {
+                continue;
             }
-            catch(NumberFormatException ex) {
-                // Add number
-                if(digits.size() > 0) {
-                    // Handle negatives
-                    String number = i < (digits.size() + 1) ? "": (input.charAt(i - (digits.size() + 1)) == '-' ? "-" : "");
+            inputArray.add(input.charAt(i) + "");
+        }
 
-                    for(int j = 0; j < digits.size(); j++) { // Combine digits to make one number
-                        number += digits.get(j).intValue() + "";
-                    }
-
-                    digits.clear();
-                    parsed.add(number);
-                }
-                // Add operator
-                else if((parsed.size() > 0 || previousAnswer != null) && !operatorFound && input.charAt(i) != ' ') { 
-                    parsed.add(input.charAt(i) + ""); // Add operator
-                    input.deleteCharAt(i--); // Remove operator so that they're not duplicated
-                    operatorFound = true;
-                }
-            }
-
-            // Add last number
-            if(i == input.length() - 1 && digits.size() > 0) {
-                // Handle negatives
-                String number =  input.charAt(i - (digits.size())) == '-' ? "-": "";
-
-                for(int j = 0; j < digits.size(); j++) { // Combine digits to make one number
-                    number += digits.get(j).intValue() + "";
-                }
-
-                digits.clear();
-                parsed.add(number);
+        // Make sure array actually contains an operator
+        boolean containsOperator = false;
+        for(int i = 0; i < inputArray.size() - 1; i++) {
+            if(!isNumber(inputArray.get(i)) && inputArray.get(i) != ".") {
+                containsOperator = true;
             }
         }
+        // If no operator is found, or if an operator is found at the end of input, return null
+        if(!containsOperator || !isNumber(inputArray.get(inputArray.size() - 1))) {
+            return null;
+        }
+
+        String digits = "0"; // Store digits
+        ArrayList<String> parsed = new ArrayList<>(); // Store fully parsed numbers
+
+        // Parse numbers
+        for(int i = 0; i < inputArray.size(); i++) {
+            if(inputArray.get(i).equals(".") && digits.contains(".")) { // Number contains two decimals, abort.
+                return null;
+            }
+            else if(isNumber(inputArray.get(i)) || inputArray.get(i).equals(".")) { // Add digit or decimal
+                digits += inputArray.get(i);
+            }
+            else if(digits.length() > 1) { // Add first number (once a non-numerical value is encountered)
+                parsed.add(digits);
+                digits = "0"; // Add leading zero for input that starts with a decimal
+            }
+
+            if(digits.length() > 0 && i == inputArray.size() - 1) { // Add last number
+                parsed.add(digits);
+                digits = "0";
+            }
+        }
+
+        // Add negatives to parsed numbers
+        for(int i = 0; i < inputArray.size(); i++) {
+            try {
+                if((inputArray.get(i).equals("-") && i != inputArray.size() - 1) 
+                    && (isNumber(inputArray.get(i + 1)) && !isNumber(inputArray.get(i - 1)))){
+                    parsed.set(parsed.size() - 1, "-" + parsed.get(parsed.size() - 1));
+                    inputArray.set(i, "0");
+                }
+            }
+            catch (IndexOutOfBoundsException ex) {
+                if(i == 0 && parsed.size() > 1) {
+                    digits = "-" + digits;
+                    inputArray.set(i, "0");
+                }
+            }
+        }
+
+        // Find Operator, if multiple operators are found, return null
+        boolean operatorFound = false;
+        for(int i = 0; i < inputArray.size(); i++) {
+            if(!isNumber(inputArray.get(i)) && !inputArray.get(i).equals(".")){
+                if(operatorFound == true) {
+                    return null;
+                }
+                parsed.add(inputArray.get(i));
+                operatorFound = true;
+            }
+        }
+
         return parsed;
     }
 
@@ -133,6 +169,16 @@ public class Calculator {
             throw new ArithmeticException("Cannot divide by zero.");
         }
         return dividend / divisor;
+    }
+
+    private static boolean isNumber(String string) {
+        try {
+            Double.parseDouble(string);
+            return true;
+        }
+        catch (NumberFormatException ex) {
+            return false;
+        }
     }
 
     private static void displayHelp() {
@@ -146,8 +192,8 @@ public class Calculator {
          [*] Multiplication
          [/] Division
         --- How to Use Operators ---
-         Enter two values and an operator between them, separated by a space.
-         Example formatting for an operation: 4 * 6
+         Example formatting for an operation: .4 * -6.2
+         Example formatting for an operation made to the previous answer: + 5
         """);
     }
 }
--- Lab_6_2/src/lab_6_2/Lab_6_2.java
+++ Lab_6_2/src/lab_6_2/Lab_6_2.java
@@ -2,8 +2,7 @@ package lab_6_2;
 
 public class Lab_6_2 {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        calculator.runCalculatorApp();
+        Calculator.runCalculatorApp();
         //Union.runUnion();
     }
 }
