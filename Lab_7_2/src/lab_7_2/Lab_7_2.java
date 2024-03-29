5e07018c3c7c01bb5ee87227781a9392915e3c83
Adam Johnston <adamjohnston144@gmail.com>
2024-03-28 23:21:44 -0400

    finished lab 7.2

--- Lab_7_2/src/lab_7_2/Lab_7_2.java
+++ Lab_7_2/src/lab_7_2/Lab_7_2.java
@@ -7,40 +7,152 @@ import java.util.InputMismatchException;
 
 public class Lab_7_2 {
     public static void main(String[] args) {
-        Q1();
+        Scanner input = new Scanner(System.in);
+        
+        while(true){ // Run app (with menu)
+            System.out.println("--- Demos ---\n [1] Random Integers\n [2] Random Doubles\n [3] Animal Names\n [4] Exit");
+            // Get choice of demo from user
+            try {
+                switch (input.nextInt()) {
+                    case 1:
+                        Q1();
+                        break;
+                    case 2:
+                        Q2();
+                        break;
+                    case 3:
+                        Q3();
+                        break;
+                    case 4:
+                        input.close();
+                        System.exit(0);
+                        break;
+                    default:
+                        throw new InputMismatchException();
+                }
+            }
+            catch(InputMismatchException ex) {
+                System.out.println("Invalid choice. \nPlease enter an integer between 1 and 5.");
+                input.nextLine();
+            }
+            System.out.println();
+        }
     }
 
-    private static ArrayList<Integer> readIntsFromFile(File file) {
-        Scanner scanner;
+    private static void Q1() {
+        String content = "";
+        for(int i = 0; i < 10; i++) {
+            content += (int)(Math.random() * 100 + 1) + " ";
+        }
 
-        try {
-            scanner = new Scanner(file);
+        ArrayList<Double> values = readNumbersFromFile(overwriteFile("Lab_7_2/Q1.txt", content));
+        
+        int sum = 0;
+        for(double num : values) {
+            sum += num;
+        }
+
+        System.out.println("Sum of random integers: " + sum);
+    }
+
+    private static void Q2() {
+        String content = "";
+        for(int i = 0; i < 15; i++) {
+            content += (Math.random() * 100 + 1) + " ";
+        }
+
+        ArrayList<Double> values = readNumbersFromFile(overwriteFile("Lab_7_2/Q2.txt", content));
+
+        double max = values.get(0), min = values.get(0);
+        for(double num : values) {
+            if(num > max) {
+                max = num;
+            }
+            else if(num < min) {
+                min = num;
+            }
+        }
+
+        System.out.println("Max of random doubles: " + max + "\nMin of random doubles: " + min);
+    }
+
+    private static void Q3() {
+        // Initial setup
+        ArrayList<String> names = readStringsFromFile(overwriteFile("Lab_7_2/Q3.txt", "jaguar tiger dog elephant iguana crane piranha cat opossum bear"));
+        
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter the name of some animals."); // Prompt user
+        while(true) {
+            String newName = userInput.next().toLowerCase();
+
+            if(newName.equals("exit")) { // Exit loop
+                break;
+            }
+
+            if(names.contains(newName)) { // If item is found, do nothing
+                System.out.println("Success: \"" + newName + "\" was found!");
+                continue;
+            }
+
+            // Item was not found, add to array list
+            System.out.println("\"" + newName + "\" was not found! Adding it to the list...");
+            names.add(newName); // Add to array list
+            
+            // Convert array list to string
+            String content = "";
+            for(String name : names) {
+                content += name + " ";
+            }
+
+            // Overwrite file with updated content
+            overwriteFile("Lab_7_2/Q3.txt", content);
+        }
+        userInput.close();
+    }
+
+    // Gather individual words from a text file into an array list
+    private static ArrayList<String> readStringsFromFile(File file) {
+        ArrayList<String> content = new ArrayList<>();
+
+        try(Scanner scanner = new Scanner(file)) {
+            while(scanner.hasNext()) {
+                content.add(scanner.next());
+            }
         }
         catch(FileNotFoundException ex) {
             System.out.println(ex);
             return null;
         }
-        ArrayList<Integer> content = new ArrayList<>();
-
-        while(scanner.hasNext()) {
-            try {
-                content.add(scanner.nextInt());
-            }
-            catch(InputMismatchException ex){
-                scanner.next();
-            }
-        }
-        scanner.close();
         return content;
     }
 
+    // Gather numbers from a text file into an array list
+    private static ArrayList<Double> readNumbersFromFile(File file) {
+        ArrayList<Double> content = new ArrayList<>();
+
+        try(Scanner scanner = new Scanner(file)) {
+            while(scanner.hasNext()) {
+                try { // Add number
+                    content.add(scanner.nextDouble());
+                }
+                catch(InputMismatchException ex){
+                    scanner.next(); // If it's not a number, skip it
+                }
+            }
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(ex);
+            return null;
+        }
+        return content;
+    }
+
+    // Overwrites file with given content, or creates new file if file does not exist
     private static File overwriteFile(String filepath, String content) {
         File file = new File(filepath);
         
-        try {
-            PrintWriter writer = new PrintWriter(file);
+        try(PrintWriter writer = new PrintWriter(file)) {
             writer.print(content);
-            writer.close();  
         }
         catch(FileNotFoundException ex) {
             System.out.println("Failed to overwrite file.");
@@ -48,44 +160,4 @@ public class Lab_7_2 {
         }
         return file;
     }
-
-    // --- Q1 ---
-    private static void Q1() {
-        ArrayList<Integer> content = readIntsFromFile(saveRandNumbers(100, 1, 10, "Q1.txt"));
-        
-        int sum = 0;
-        for(int i = 0; i < content.size(); i++) {
-            sum += content.get(i);
-        }
-
-        System.out.println("Sum of random integers: " + sum);
-    }
-
-    private static File saveRandNumbers(int high, int offset, int numValues, String filepath) {
-        int[] numbers = genRandIntegers(high, offset, numValues);
-        String content = "";
-
-        for(int number : numbers) {
-            content += number + " ";
-        }
-
-        return overwriteFile(filepath, content);
-    }
-
-    private static int[] genRandIntegers(int high, int offset, int numValues) {
-        int[] values = new int[numValues];
-        for(int i = 0; i < numValues; i++) {
-            values[i] = (int)((Math.random() * high) + offset);
-        }
-        return values;
-    }
-
-    // --- Q2 ---
-    // private static int[] genRandDoubles(double high, double offset, int numValues) {
-    //     double[] values = new double[numValues];
-    //     for(int i = 0; i < numValues; i++) {
-    //         values[i] = (int)((Math.random() * high) + offset);
-    //     }
-    //     return values;
-    // }
 }
